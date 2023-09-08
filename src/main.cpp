9809ecd36d1ead52214ff6701a7936f5e3e99617
adiakimson <karolinajoachimczyk@wp.pl>
2023-09-08 21:41:07 +0200

    check_and_test

--- src/main.cpp
+++ src/main.cpp
@@ -11,11 +11,16 @@ adres 2
 adres 3
 4. Pion 4 
 adres 4
-
 opcjonalny adres mastera : 0
 */
 
-#define I2C_ADDR1 (1) //pion 1
+//piny od mega 2,3,18,19,20,21 są od przerwań
+//20 SDA, 21 SCL
+
+#define SDA (20) //SDA master default
+#define SCL (21) //SCL master default
+
+#define I2C_ADDR1 (1) //pion 1 - wartości zawsze 7bitowe od 0 do 127
 #define I2C_ADDR2 (2) //pion 2
 #define I2C_ADDR3 (3) //pion 3
 #define I2C_ADDR4 (4) //pion 4
@@ -49,6 +54,7 @@ int event3_ctr = 0;
 int event4_ctr = 0;
 
 //obsługa przerwań od pionów - załączenie flagi
+
 void Isr1()
 {
     flag1 = true;
@@ -69,52 +75,74 @@ void Isr4()
     flag4 = true;
 }
 
+//komunikacja od mastera do rpi - USB (UART 0)
+
+void MasterToRpi()
+{
+    Serial.begin(115200); //baudrate między master a rpi
+    Serial.println("Otwarto komunikację Master - RaspberryPi");
+}
+
 //obsługa komunikacji i2c jako master
 
 void start_I2C()
 {
-    Wire.begin();
+    Wire.begin(); //tutaj adres mastera w razie czego
     Wire.write("Tutaj master Arduino!");
 }
+
+//reset magistrali w razie problemów
+
+void ResetI2C()
+{
+    TWCR = 0;
+    Wire.begin();
+}
+
+//wykrycie zdarzenia od slave
 
 void eventOccured(int pion)
 {
     switch(pion)
     {
         case 1:
-        while(Wire.available())
+        event1_ctr++;
+        while(Wire.available()!=0) //wykonuje JEŚLI są bajty do odczytu
         {
-            event1_ctr++;
-            Wire.requestFrom(1,1); //oczekuje od pionu 1 1 byte
-            //?
+            char mssg = Wire.read(); //odczytuje i przechowuje wiadomość
+            Serial.println(mssg); //przekazuje wiadomość do rpi
         }
+        flag1 = false; //opuszczenie flagi po odczycie wszystkiego
         break;
         case 2:
-        while(Wire.available())
+        event2_ctr++;
+        while(Wire.available()!=0) //wykonuje JEŚLI są bajty do odczytu
         {
-            event2_ctr++;
-            Wire.requestFrom(2,1); //oczekuje od pionu 2 1 byte
-            //?
+            char mssg = Wire.read(); //odczytuje i przechowuje wiadomość
+            Serial.println(mssg); //przekazuje wiadomość do rpi
         }
+        flag2 = false; //opuszczenie flagi po odczycie wszystkiego
         break;
         case 3:
-        while(Wire.available())
+        event3_ctr++;
+        while(Wire.available()!=0) //wykonuje JEŚLI są bajty do odczytu
         {
-            event3_ctr++;
-            Wire.requestFrom(3,1); //oczekuje od pionu 3 1 byte
-            //?
+            char mssg = Wire.read(); //odczytuje i przechowuje wiadomość
+            Serial.println(mssg); //przekazuje wiadomość do rpi
         }
+        flag3 = false; //opuszczenie flagi po odczycie wszystkiego
         break;
         case 4:
-        while(Wire.available())
-        {
-            event4_ctr++;
-            Wire.requestFrom(4,1); //oczekuje od pionu 4 1 byte
-            //?
+        event4_ctr++;
+        while(Wire.available()!=0) //wykonuje JEŚLI są bajty do odczytu
+        { 
+            char mssg = Wire.read(); //odczytuje i przechowuje wiadomość
+            Serial.println(mssg); //przekazuje wiadomość do rpi
         }
+        flag4 = false; //opuszczenie flagi po odczycie wszystkiego
         break;
         default:
-        //?
+        Serial.println("Wykryto inne zdarzenie na magistrali I2C");
         break;
     }
     
@@ -162,15 +190,16 @@ void MotorRequest()
     {
         DownMove();
     }
-    else //jeśli oba na raz lub żaden
+    else //jeśli oba na raz lub żaden ustawiam oba na stan niski
     {
-        //?
+        digitalWrite(STEP_M1, LOW);
+        digitalWrite(STEP_M2, LOW);
     }
 }
 
 void setup() {
 
-//informacja startowa
+//informacja startowa do podglądu
 Serial.begin(115200);
 Serial.println("Uruchomiono master Arduino!");
 
@@ -180,21 +209,66 @@ pinMode(I2C_INTERRUPT_2, INPUT_PULLUP);
 pinMode(I2C_INTERRUPT_3, INPUT_PULLUP);
 pinMode(I2C_INTERRUPT_4, INPUT_PULLUP);
 
-//konfiguracja pinów ste pmotoru
+//konfiguracja pinów step motoru
 pinMode(STEP_M1, OUTPUT);
 pinMode(STEP_M2, OUTPUT);
 
-//ustanowienie przerwań
+//wartość początkowa dla pinów step motoru
+digitalWrite(STEP_M1, LOW);
+digitalWrite(STEP_M2, LOW);
+
+//ustanowienie przerwań - w razie potrzeby zmienić logike
 attachInterrupt(2,Isr1,FALLING);
 attachInterrupt(3,Isr2,FALLING);
 attachInterrupt(18,Isr3,FALLING);
 attachInterrupt(19,Isr4,FALLING);
 
-
-
+//start komunikacji
+MasterToRpi(); //do rpi
+start_I2C(); //do peryferiów
 }
 
 void loop() {
 
-  
+//oczekiwanie na polecenie dotyczące step motoru
+if(req_up == true)
+{
+    UpMove();
+    req_up = false;
+    Serial.println("Wykonano polecenie w górę");
+}
+
+if(req_down == true)
+{
+    DownMove();
+    req_down = false;
+    Serial.println("Wykonano polecenie w dół");
+}
+
+//oczekiwanie na sygnały od slave
+if(flag1 == true)
+{
+    Serial.println("Wykryto żądanie od pionu 1"); //komunikat dla rpi
+    eventOccured(1); //obsługa żądania od slave
+}
+else if(flag2 == true)
+{
+    Serial.println("Wykryto żądanie od pionu 2"); //komunikat dla rpi
+    eventOccured(2); //obsługa żądania od slave
+}
+else if(flag3 == true)
+{
+    Serial.println("Wykryto żądanie od pionu 3"); //komunikat dla rpi
+    eventOccured(3); //obsługa żądania od slave
+}
+else if(flag4 == true)
+{
+    Serial.println("Wykryto żądanie od pionu 4"); //komunikat dla rpi
+    eventOccured(4); //obsługa żądania od slave
+}
+else
+{
+    Serial.println("Brak wiadomości od peryferiów"); //tymczasowo w celach debug
+}
+
 }
